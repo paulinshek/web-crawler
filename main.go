9d37205d7fc26116d372a3b74c723e1123917733
paulin.shek@gmail.com <paulin.shek@gmail.com>
2018-10-20 00:13:32 +0100

    Simple crawler is working now
    * Spin up a test server to test simple cases of only a few pages
    * Fix all the bugs related to not closing channels and blocking etc.
    * Crawler now will traverse through pages and return a response

--- main.go
+++ main.go
@@ -10,43 +10,72 @@ import (
 	"strings"
 )
 
-func everythingHandler(w http.ResponseWriter, r *http.Request) {
-	domain := "https://monzo.com"
-	filterAndTransformLinks := filterOrResolve(domain)
+func startTestServer() {
+	h := http.NewServeMux()
+	h.HandleFunc("/test", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "<a href=\"/another-page\">my link</a>")
+	})
+	h.HandleFunc("/another-page", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "yayyy")
+	})
+	log.Fatal(http.ListenAndServe(":8080", h))
+}
 
-	unexploredUrls := make(chan string)
-	go func() {
-		unexploredUrls <- domain // start
-	}()
+func startWebcrawlerServer() {
+	h := http.NewServeMux()
+	h.HandleFunc("/", startCrawling)
+	log.Fatal(http.ListenAndServe(":8000", h))
+}
+
+func startCrawling(w http.ResponseWriter, r *http.Request) {
+	domain := "http://localhost:8080/test"
+	seenBefore := make(map[string]struct{})
+	seenBefore[domain] = struct{}{}
+	crawlFrom(domain, seenBefore, domain, w)
+}
+
+func crawlFrom(domain string, seenBefore map[string]struct{}, currLink string, w http.ResponseWriter) {
 
 	foundHyperlinks := make(chan string)
-	go exploreForLinks(unexploredUrls, foundHyperlinks)
-
+	go exploreForLinks(currLink, foundHyperlinks)
 	resolvedUrlsInDomain := make(chan string)
-	go filterAndTransformLinks(foundHyperlinks, resolvedUrlsInDomain)
+	go filterExternalOrResolve(domain)(foundHyperlinks, resolvedUrlsInDomain)
 
-	go rerouteUnexploredLinks(resolvedUrlsInDomain, unexploredUrls)
+	for resolvedUrl := range resolvedUrlsInDomain {
+		if _, ok := seenBefore[resolvedUrl]; !ok { // if no seen before <-> not explored before
+			seenBefore[resolvedUrl] = struct{}{} // mark it as seen
+
+			crawlFrom(domain, seenBefore, resolvedUrl, w)
+		}
+	}
+	fmt.Println(w, currLink)
 }
 
 func main() {
-	http.HandleFunc("/", everythingHandler)
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	go startTestServer()
+
+	go startWebcrawlerServer()
+
+	resp, err := http.Get("http://localhost:8000/")
+	if err == nil {
+		defer resp.Body.Close()
+		bodyAsByteArray, _ := ioutil.ReadAll(resp.Body)
+		fmt.Println(string(bodyAsByteArray[:]))
+	}
 }
 
-func exploreForLinks(in chan string, out chan string) {
-	for url := range in {
-		resp, err := http.Get(url)
+func exploreForLinks(currUrl string, out chan string) {
+	resp, err := http.Get(currUrl)
+	if err == nil {
 		defer resp.Body.Close()
-		if err == nil {
-			bodyAsByteArray, _ := ioutil.ReadAll(resp.Body)
-			doc, _ := html.Parse(bytes.NewReader(bodyAsByteArray))
+		bodyAsByteArray, _ := ioutil.ReadAll(resp.Body)
+		doc, _ := html.Parse(bytes.NewReader(bodyAsByteArray))
+		pushHyperlinksToChannelRecursively(doc, out)
 
-			pushHyperlinksToChannelRecursively(doc, out)
-
-		} else {
-			fmt.Println("ERROR: %s", err)
-		}
+	} else {
+		fmt.Println("ERROR: %s", err)
 	}
+	close(out)
 }
 
 func pushHyperlinksToChannelRecursively(n *html.Node, rawHyperlinkReceiver chan string) {
@@ -62,20 +91,7 @@ func pushHyperlinksToChannelRecursively(n *html.Node, rawHyperlinkReceiver chan 
 	}
 }
 
-func rerouteUnexploredLinks(in chan string, out chan string) {
-	foundUrls := make(map[string]struct{})
-	for resolvedUrl := range in {
-		if _, ok := foundUrls[resolvedUrl]; !ok {
-			fmt.Println(resolvedUrl)
-			foundUrls[resolvedUrl] = struct{}{}
-			go func() {
-				out <- resolvedUrl
-			}()
-		}
-	}
-}
-
-func filterOrResolve(domain string) func(in chan string, out chan string) {
+func filterExternalOrResolve(domain string) func(in chan string, out chan string) {
 	return func(in chan string, out chan string) {
 		for url := range in {
 			if strings.HasPrefix(url, "/") {
@@ -84,6 +100,7 @@ func filterOrResolve(domain string) func(in chan string, out chan string) {
 				out <- removeFragmentIdentifier(url)
 			}
 		}
+		close(out)
 	}
 }
 
@@ -94,4 +111,3 @@ func removeFragmentIdentifier(url string) string {
 // BUG printed urls are not completely unique
 // TODO parent link should also be pushed to channel so that we can create a graph from this Data
 // TODO actually return something
-// TODO refactor to use in/out channels
