4f18f12ab0f6a18c0640cc76d9a3e08a27944f40
paulin.shek@gmail.com <paulin.shek@gmail.com>
2018-10-25 22:04:46 +0100

    Fix and improve tests

--- smoke_test.go
+++ smoke_test.go
@@ -38,11 +38,11 @@ func TestOneTwoPages(t *testing.T) {
 	})
 	go http.ListenAndServe(":8080", h)
 
-	actualDotOutput := startWebcrawler("http://localhost:8080")
+	actualDotOutput := startWebcrawler("http://localhost:8080/test")
 	actualDotOutput = strings.Replace(actualDotOutput, " ", "", -1)
 	actualDotOutput = strings.Replace(actualDotOutput, "\n", "", -1)
 	actualDotOutput = strings.Replace(actualDotOutput, "\t", "", -1)
-	expectedDotOutput := "digraph{node[label=\"http://localhost:8080\"]n1;}"
+	expectedDotOutput := "digraph{node[label=\"http://localhost:8080/test\"]n1;node[label=\"http://localhost:8080/test/another-page\"]n2;n1->n2;}"
 
 	if expectedDotOutput != actualDotOutput {
 		t.Errorf("Web crawler was incorrect, got: <%s>, want: <%s>.", actualDotOutput, expectedDotOutput)
@@ -60,14 +60,21 @@ func TestOneTwoPagesWithLoop(t *testing.T) {
 	})
 	go http.ListenAndServe(":8080", h)
 
-	actualDotOutput := startWebcrawler("http://localhost:8080")
+	actualDotOutput := startWebcrawler("http://localhost:8080/test")
 	actualDotOutput = strings.Replace(actualDotOutput, " ", "", -1)
 	actualDotOutput = strings.Replace(actualDotOutput, "\n", "", -1)
 	actualDotOutput = strings.Replace(actualDotOutput, "\t", "", -1)
-	expectedDotOutput := "digraph{node[label=\"http://localhost:8080\"]n1;}"
 
-	if expectedDotOutput != actualDotOutput {
-		t.Errorf("Web crawler was incorrect, got: <%s>, want: <%s>.", actualDotOutput, expectedDotOutput)
+	if !strings.Contains(actualDotOutput, "node[label=\"http://localhost:8080/test\"]") {
+		t.Errorf("Web crawler did not create node[label=\"http://localhost:8080/test\"]")
 	}
-
+	if !strings.Contains(actualDotOutput, "node[label=\"http://localhost:8080/test/another-page\"]") {
+		t.Errorf("Web crawler did not create node[label=\"http://localhost:8080/test/another-page\"]")
+	}
+	if !strings.Contains(actualDotOutput, "n1->n2;") {
+		t.Errorf("Web crawler did not create link between the two nodes")
+	}
+	if strings.Contains(actualDotOutput, "n3") {
+		t.Errorf("Web crawler found too many nodes")
+	}
 }
