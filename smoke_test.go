e924f13688c84247ac98067d32c03d6c2c395286
James Grant <jd.grant29@gmail.com>
2019-05-30 22:16:09 +0100

    Simplify test examples

--- smoke_test.go
+++ smoke_test.go
@@ -9,11 +9,8 @@ import (
 
 func TestOnePage(t *testing.T) {
 	h := http.NewServeMux()
-	h.HandleFunc("/test", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "<a href=\"http://localhost:8080/test/another-page\">my link</a>")
-	})
-	h.HandleFunc("/another-page", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "<a href=\"http://localhost:8080/test\">my link</a>")
+	h.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "<html></html>")
 	})
 	go http.ListenAndServe(":8080", h)
 
@@ -30,19 +27,19 @@ func TestOnePage(t *testing.T) {
 
 func TestOneTwoPages(t *testing.T) {
 	h := http.NewServeMux()
-	h.HandleFunc("/test", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "<a href=\"http://localhost:8080/test/another-page\">my link</a>")
+	h.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "<a href=\"http://localhost:8080/another-page\">my link</a>")
 	})
 	h.HandleFunc("/another-page", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "<a href=\"http://localhost:8080/test\">my link</a>")
+		fmt.Fprintf(w, "<html></html>")
 	})
 	go http.ListenAndServe(":8080", h)
 
-	actualDotOutput := startWebcrawler("http://localhost:8080/test", "http://localhost:8080").String()
+	actualDotOutput := startWebcrawler("http://localhost:8080/", "http://localhost:8080").String()
 	actualDotOutput = strings.Replace(actualDotOutput, " ", "", -1)
 	actualDotOutput = strings.Replace(actualDotOutput, "\n", "", -1)
 	actualDotOutput = strings.Replace(actualDotOutput, "\t", "", -1)
-	expectedDotOutput := "digraph{node[label=\"http://localhost:8080/test\"]n1;node[label=\"http://localhost:8080/test/another-page\"]n2;n1->n2;}"
+	expectedDotOutput := "digraph{node[label=\"http://localhost:8080\"]n1;node[label=\"http://localhost:8080/another-page\"]n2;n1->n2;}"
 
 	if expectedDotOutput != actualDotOutput {
 		t.Errorf("Web crawler was incorrect, got: <%s>, want: <%s>.", actualDotOutput, expectedDotOutput)
@@ -50,31 +47,21 @@ func TestOneTwoPages(t *testing.T) {
 
 }
 
-func TestOneTwoPagesWithLoop(t *testing.T) {
+func TestLoop(t *testing.T) {
 	h := http.NewServeMux()
-	h.HandleFunc("/test", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "<a href=\"http://localhost:8080/test/another-page\">my link</a>")
-	})
-	h.HandleFunc("/another-page", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "<a href=\"http://localhost:8080/test\">my link</a>")
+	h.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "<a href=\"http://localhost:8080/\">back to me</a>")
 	})
 	go http.ListenAndServe(":8080", h)
 
-	actualDotOutput := startWebcrawler("http://localhost:8080/test", "http://localhost:8080").String()
+	actualDotOutput := startWebcrawler("http://localhost:8080/", "http://localhost:8080").String()
 	actualDotOutput = strings.Replace(actualDotOutput, " ", "", -1)
 	actualDotOutput = strings.Replace(actualDotOutput, "\n", "", -1)
 	actualDotOutput = strings.Replace(actualDotOutput, "\t", "", -1)
 
-	if !strings.Contains(actualDotOutput, "node[label=\"http://localhost:8080/test\"]") {
-		t.Errorf("Web crawler did not create node[label=\"http://localhost:8080/test\"]")
-	}
-	if !strings.Contains(actualDotOutput, "node[label=\"http://localhost:8080/test/another-page\"]") {
-		t.Errorf("Web crawler did not create node[label=\"http://localhost:8080/test/another-page\"]")
-	}
-	if !strings.Contains(actualDotOutput, "n1->n2;") {
-		t.Errorf("Web crawler did not create link between the two nodes")
-	}
-	if strings.Contains(actualDotOutput, "n3") {
-		t.Errorf("Web crawler found too many nodes")
+	expectedDotOutput := "digraph{node[label=\"http://localhost:8080\"]n1;n1->n1;}"
+
+	if expectedDotOutput != actualDotOutput {
+		t.Errorf("Web crawler was incorrect, got: <%s>, want: <%s>.", actualDotOutput, expectedDotOutput)
 	}
 }
