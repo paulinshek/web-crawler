0859d610aaaf3435bff5f71f6a9ebb769f5462f1
paulin.shek@gmail.com <paulin.shek@gmail.com>
2019-06-03 22:08:12 +0100

    Add tests for fragment identifier and relative links

--- smoke_test.go
+++ smoke_test.go
@@ -107,3 +107,55 @@ func TestLinkToDifferentDomain(t *testing.T) {
 		t.Errorf("Web crawler was incorrect, got: <%s>, want: <%s>.", actualDotOutput, expectedDotOutput)
 	}
 }
+
+func TestRelativeLink(t *testing.T) {
+	h := http.NewServeMux()
+	h.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "<html><a href=\"/another-page\">relative link</a></html>")
+	})
+	h.HandleFunc("/another-page", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "<html></html>")
+	})
+	server := &http.Server{
+		Addr:    ":8080",
+		Handler: h,
+	}
+	defer server.Shutdown(context.Background())
+	go server.ListenAndServe()
+
+	actualDotOutput := startWebcrawler("http://localhost:8080/", "http://localhost:8080").String()
+	actualDotOutput = strings.Replace(actualDotOutput, " ", "", -1)
+	actualDotOutput = strings.Replace(actualDotOutput, "\n", "", -1)
+	actualDotOutput = strings.Replace(actualDotOutput, "\t", "", -1)
+
+	expectedDotOutput := "digraph{node[label=\"http://localhost:8080/\"]n1;node[label=\"http://localhost:8080/another-page\"]n2;n1->n2;}"
+	if expectedDotOutput != actualDotOutput {
+		t.Errorf("Web crawler was incorrect, got: <%s>, want: <%s>.", actualDotOutput, expectedDotOutput)
+	}
+}
+
+func TestFragmentIdentifier(t *testing.T) {
+	h := http.NewServeMux()
+	h.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "<html><a href=\"http://localhost:8080/another-page#14254464\">relative link</a></html>")
+	})
+	h.HandleFunc("/another-page", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "<html></html>")
+	})
+	server := &http.Server{
+		Addr:    ":8080",
+		Handler: h,
+	}
+	defer server.Shutdown(context.Background())
+	go server.ListenAndServe()
+
+	actualDotOutput := startWebcrawler("http://localhost:8080/", "http://localhost:8080").String()
+	actualDotOutput = strings.Replace(actualDotOutput, " ", "", -1)
+	actualDotOutput = strings.Replace(actualDotOutput, "\n", "", -1)
+	actualDotOutput = strings.Replace(actualDotOutput, "\t", "", -1)
+
+	expectedDotOutput := "digraph{node[label=\"http://localhost:8080/\"]n1;node[label=\"http://localhost:8080/another-page\"]n2;n1->n2;}"
+	if expectedDotOutput != actualDotOutput {
+		t.Errorf("Web crawler was incorrect, got: <%s>, want: <%s>.", actualDotOutput, expectedDotOutput)
+	}
+}
